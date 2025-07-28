622609f485b3a70fd4a79278b2842fbc0110cbab
SupMan <mansupmansup232@gmail.com>
2025-07-29 00:17:39 +0530

    MEssi GOAT

---
+++ dijkstra.c
@@ -0,0 +1,73 @@
+#include<stdio.h>
+#define inf 999
+    int n;
+    int g[10][10];
+    int c[10][10];
+    int d[10];
+    int visit[10];
+    int start;
+    int mind;
+    int nextnode;
+    void dijkstra()
+        {   for(int i=0;i<n;i++)
+            {
+                d[i]=c[start][i];
+                visit[i]=0;
+            }
+            d[start]=0;
+            visit[start]=1;
+            int count=1;
+            while(count<n-1)
+            {mind=inf;
+              for(int i=0;i<n;i++)
+                  {
+                    if(d[i]<mind && !visit[i])
+                    {    mind=d[i];
+                         nextnode=i;
+                  }
+                }
+                visit[nextnode]=1;
+                for(int k=0;k<n;k++)
+                    {for(int i=0;i<n;i++){
+                        if(!visit[i])
+                        {
+                            if(mind+c[nextnode][i]<d[i])
+                                d[i]=mind+c[nextnode][i];
+                        }
+                    }}
+                  count++;
+            }
+            for (int i=0;i<n;i++)
+                {
+                    if(i!=start)
+                        printf("distance of source to node %d is : %d\n",i,d[i]);
+                        
+                }
+
+        }
+    int main()
+    {
+        FILE *fp;
+        fp=fopen("djk.txt","r");
+        if(fp==NULL)
+            printf("ERR");
+        else    
+            fscanf(fp,"%d",&n);
+        for(int i=0;i<n;i++)
+            {for(int j=0;j<n;j++){
+
+            
+                fscanf(fp,"%d",&g[i][j]);  
+                if(g[i][j]==0)
+                    c[i][j]=inf;
+                else
+                    c[i][j]=g[i][j];
+
+            }
+        }
+        printf("Enter source node:\t");
+        scanf("%d",&start);
+        dijkstra();
+
+        
+    }
---
+++ quicksort.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+int a[]={35,10,100,32,21,56,99,47};
+int n=sizeof(a)/sizeof(int);
+
+void swap(int* a, int* b)
+{   int temp=*a;
+    *a=*b;
+    *b=temp;
+} 
+int partition(int l, int h)
+{
+    int i=l+1;
+    int pivot=a[l];
+    int j=h;
+    while (1)
+    {
+            while(a[i]<=pivot && i<=h) i++;
+            while(a[j]>pivot && j>=l) j--;
+       
+             if(i<j)
+          swap(&a[i],&a[j]);
+        else
+            break;
+     
+    }
+    
+        swap(&a[l],&a[j]);
+return j;
+    
+    }
+
+
+void quicksort(int l,int h)
+{   
+    if(l<h){
+    int pivot=partition(l,h);
+    quicksort(l,pivot-1);
+    quicksort(pivot+1,h);
+}}
+int main()
+{   printf("Start\n");
+    quicksort(0,n-1);
+    for(int i = 0;i<n;i++)
+        printf(" %d ",a[i]);
+}
